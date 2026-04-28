2be3bdc8ec763e689d6e7165442d29bc10a7a34b
K@run <karunkumar02525@gmail.com>
2026-04-28 16:55:27 +0530

    Replace ContentView with CustomView in app entry

--- Sources/QRCodeScanner/Test/ScannerDemoAppApp.swift
+++ Sources/QRCodeScanner/Test/ScannerDemoAppApp.swift
@@ -4,15 +4,16 @@
 //
 //  Created by KARUN KUMAR on 25/04/26.
 //
-
+/*
 import SwiftUI
 
 @main
 struct ScannerDemoAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CustomView()
            // BuiltinOverlay()
         }
     }
 }
+*/
